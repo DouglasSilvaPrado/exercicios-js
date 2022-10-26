701353cbec92a3482dca6e248fa91d670825a74e
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-10-26 15:16:04 -0300

    add triangle exercise with loop

--- Triangulo com Loop/code.js
+++ Triangulo com Loop/code.js
@@ -1,5 +1,12 @@
 function loop() {
     // code
+    let hash = '#'
+    for (let index = 1; index <= 7; index++){
+        console.log(hash);
+        hash += '#'
+    }
 }
+
+loop()
 
 module.exports = loop
