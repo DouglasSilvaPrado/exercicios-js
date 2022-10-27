d936e955893291552a30906be24756860e8290f5
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-10-27 13:57:54 -0300

    add min exercise

--- Minimo/code.js
+++ Minimo/code.js
@@ -1,5 +1,10 @@
-function minimo() {
-    // code
+function minimo(num1, num2) {
+   if(num1 < num2) return num1;
+   return num2;
 }
+
+console.log(minimo(0, 10));
+
+console.log(minimo(0, -10));
 
 module.exports = minimo
