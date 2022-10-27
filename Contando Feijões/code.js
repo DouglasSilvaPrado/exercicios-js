b8248f3401c2c7d8eac445ecd0bc56101fcac405
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-10-27 13:58:19 -0300

    add isEven exercise

--- Contando Feijões/code.js
+++ Contando Feijões/code.js
@@ -1,11 +1,24 @@
 function countBs(word) {
-    // code
+    let count = 0
+    for (character of word) {
+        if(character === "B")count++
+    }
+    return count
 }
 
 function countChar(word, char) {
-    // code
+    let count = 0
+    for (character of word) {
+        if(character === char)count++
+    }
+    return count
 }
 
+console.log(countBs("BBC"))
+console.log(countBs("Bruno"))
+console.log(countBs("bruno"))
+console.log(countChar("kakkerlak", "k"));
+console.log(countChar("larissa", "a"));
 module.exports = {
     countBs,
     countChar
