23df50431cab1f03b3471b19a19183985f8be6b8
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-10-28 09:47:42 -0300

    add exercices class

--- Calculadora/code.js
+++ Calculadora/code.js
@@ -1,3 +1,26 @@
-// define your class here
+class Calculadora {
+  somar(num1, num2){
+    return num1 + num2
+  }
+
+  subtrair(num1, num2){
+    return num1 - num2
+  }
+
+  dividir(num1, num2){
+    return num1 / num2
+  }
+
+  multiplicar(num1, num2){
+    return num1 * num2
+  }
+}
+
+const calculadora = new Calculadora()
+
+calculadora.somar(1, 3)
+calculadora.subtrair(2, 1)
+calculadora.dividir(4, 2)
+calculadora.multiplicar(4, 2)
 
 module.exports = Calculadora
--- Helper/code.js
+++ Helper/code.js
@@ -1,3 +1,23 @@
-// define your class here
+class Helper{
 
-module.exports = NumberHelper
+  static isEven(n){
+    return n % 2 === 0 ? true: false;
+  }
+
+  static isPositive(n){
+    return n > 0 ? true : false;
+  }
+
+  static isNegative(n){
+    return n < 0 ? true : false;
+  }
+}
+
+Helper.isEven(2)
+Helper.isEven(3)
+Helper.isPositive(2)
+Helper.isPositive(-10)
+Helper.isNegative(-10)
+Helper.isNegative(10)
+
+module.exports = Helper
